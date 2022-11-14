d88db81ae1304eca3d46c9ea2873ddbe827aba5e
injashkin <injashkin@gmail.com>
2022-11-14 23:41:04 +0300

    Update gatsby and dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
       resolve: `gatsby-source-wordpress`,
       options: {
         // the only required plugin option for WordPress is the GraphQL url.
-        url: process.env.WPGRAPHQL_URL || `http://qwerty.local/graphql`,
+        url: process.env.WPGRAPHQL_URL || `http://g-wp/graphql`,
       },
     },
 
