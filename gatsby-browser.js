5acb3dcebc483d36909d54041d0afc7b17249d32
injashkin <injashkin@gmail.com>
2022-11-17 21:19:17 +0300

    Change bulma to bootstrap

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -8,7 +8,11 @@ import "./src/css/normalize.css"
 // custom CSS styles
 import "./src/css/style.css"
 
-import "./src/scss/bulma.scss"
+//import "./src/scss/bulma.scss"
+
+import "bootstrap/dist/css/bootstrap.min.css"
 
 // мои стили
-import "./src/scss/mystyles.scss"
+// import "./src/scss/mystyles.css"
+
+import "./src/scss/custom.scss"
